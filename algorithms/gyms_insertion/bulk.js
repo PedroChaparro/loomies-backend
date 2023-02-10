9972ff6b6b8e390b417d87f91e9005af87a8ca72
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-09 20:52:23 -0500

    :ambulance: Add database name
    Avoid default name

--- algorithms/gyms_insertion/bulk.js
+++ algorithms/gyms_insertion/bulk.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 dotenv.config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {});
+mongoose.connect(process.env.MONGO_URI, { dbName: "loomies" });
 
 // Mongo models
 const ZoneSchema = new mongoose.Schema(
