4e95c2ed789fd7adf0fb62b4621f130d235f3956
andres123dbh <andres123dbh@gmail.com>
2023-02-12 14:51:47 -0500

    Empty fields validated before query the database

--- api/controllers/user.go
+++ api/controllers/user.go
@@ -22,26 +22,6 @@ func HandleSignUp(c *gin.Context) {
 		return
 	}
 
-	err = models.CheckExistEmail(form.Email)
-
-	if !(err != nil) {
-		if !(err == mongo.ErrNoDocuments) {
-			c.AbortWithStatusJSON(http.StatusConflict,
-				gin.H{"message": "Email already exists"})
-			return
-		}
-	}
-
-	err = models.CheckExistUsername(form.Username)
-
-	if !(err != nil) {
-		if !(err == mongo.ErrNoDocuments) {
-			c.AbortWithStatusJSON(http.StatusConflict,
-				gin.H{"message": "Username already exists"})
-			return
-		}
-	}
-
 	if form.Username == "" {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Username cannot be empty"})
 		return
@@ -71,6 +51,26 @@ func HandleSignUp(c *gin.Context) {
 	} else {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Password is too short"})
 		return
+	}
+
+	err = models.CheckExistEmail(form.Email)
+
+	if !(err != nil) {
+		if !(err == mongo.ErrNoDocuments) {
+			c.AbortWithStatusJSON(http.StatusConflict,
+				gin.H{"message": "Email already exists"})
+			return
+		}
+	}
+
+	err = models.CheckExistUsername(form.Username)
+
+	if !(err != nil) {
+		if !(err == mongo.ErrNoDocuments) {
+			c.AbortWithStatusJSON(http.StatusConflict,
+				gin.H{"message": "Username already exists"})
+			return
+		}
 	}
 
 	hashed, err := bcrypt.GenerateFromPassword([]byte(form.Password), 8)
