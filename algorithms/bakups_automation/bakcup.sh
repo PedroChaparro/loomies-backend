6126231137398ba6af40c3eefec23e860c770205
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-11 21:16:54 -0500

    :ambulance: Fix parameters order and remove the unneded ones

--- algorithms/bakups_automation/bakcup.sh
+++ algorithms/bakups_automation/bakcup.sh
@@ -4,21 +4,22 @@
 HOSTS=$1
 BACKUP_DIR=$2
 USER=$3
-PASSWORD=$4
-DATABASE=$5
+DRIVE_FOLDER_ID=$4
+PASSWORD=$5
 
 # Get current date in format YYYY-MM-DD
 DATE=`date +%Y-%m-%d`
 echo "Creating $DATE backup ⏳"
 
 # Execute mongo dump command
-mongodump --host=$HOSTS --db $DATABASE --username $USER --password $PASSWORD --out $BACKUP_DIR/$DATE
+mongodump --host $HOSTS --out $BACKUP_DIR/$DATE --username $USER --password $PASSWORD
 echo "$DATE Backup created ✅"
 
 # Compress backup folder
-tar -zcvf $BACKUP_DIR/$DATE.tar.gz $BACKUP_DIR/$DATE
+cd $BACKUP_DIR
+tar -zcvf $DATE.tar.gz $DATE
 echo "$DATE Backup compressed 📥"
 
 # Run gdrive upload command
-gdrive upload $BACKUP_DIR/$DATE.tar.gz
+gdrive files upload $BACKUP_DIR/$DATE.tar.gz --parent $DRIVE_FOLDER_ID
 echo "$DATE Backup uploaded to Google Drive ☁️"
