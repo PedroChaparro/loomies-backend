cec3041cadd43f29cb9f711dfdaf75122c7c962a
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-13 13:12:37 -0500

    :sparkles: Script: Check status codes

--- algorithms/backups/backup.sh
+++ algorithms/backups/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Needed parameters
+# Needed parameters (passed as cli arguments)
 HOSTS=$1
 BACKUP_DIR=$2
 USER=$3
@@ -13,13 +13,34 @@ echo "Creating $DATE backup ⏳"
 
 # Execute mongo dump command
 mongodump --host $HOSTS --out $BACKUP_DIR/$DATE --username $USER --password $PASSWORD
+STATUS=$?
+
+if [ $STATUS -ne 0 ]; then
+  echo "🚨 Error creating backup"
+  exit 1
+fi
+
 echo "$DATE Backup created ✅"
 
 # Compress backup folder
 cd $BACKUP_DIR
 tar -zcvf $DATE.tar.gz $DATE
+STATUS = $?
+
+if [ $STATUS -ne 0 ]; then
+  echo "🚨 Error compressing backup"
+  exit 1
+fi
+
 echo "$DATE Backup compressed 📥"
 
 # Run gdrive upload command
 gdrive files upload $BACKUP_DIR/$DATE.tar.gz --parent $DRIVE_FOLDER_ID
+STATUS = $?
+
+if [ $STATUS -ne 0 ]; then
+  echo "🚨 Error uploading backup to Google Drive"
+  exit 1
+fi
+
 echo "$DATE Backup uploaded to Google Drive ☁️"
