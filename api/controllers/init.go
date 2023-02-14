696f796636c256b3b484680d0331657e9ec988ce
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-02-14 09:22:48 -0500

    :construction: Initial commit, finding near gyms
    Now, just finding current location

--- api/controllers/init.go
+++ api/controllers/init.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	"github.com/PedroChaparro/loomies-backend/models"
 	"github.com/gin-gonic/gin"
 )
@@ -11,4 +13,14 @@ func HandleZonesGet(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"zones": zones,
 	})
+}
+
+func HandleNearZonesGet(c *gin.Context) {
+	// temporary example
+	zone, _ := models.GetCurrentZone(-73.07220400291057, 7.038141994916382)
+	fmt.Println(zone)
+
+	/* c.JSON(200, gin.H{
+		"zone": zone,
+	}) */
 }
--- api/models/init.go
+++ api/models/init.go
@@ -28,3 +28,51 @@ func GetZones() []interfaces.Zone {
 
 	return zones
 }
+
+func GetCurrentZone(latitude float32, longitude float32) (p interfaces.Zone, e error) {
+
+	filter := bson.D{
+		{"$and",
+			bson.A{
+				bson.D{{"rightFrontier", bson.D{{"$gte", latitude}}}},
+				bson.D{{"leftFrontier", bson.D{{"$lte", latitude}}}},
+				bson.D{{"bottomFrontier", bson.D{{"$lte", longitude}}}},
+				bson.D{{"topFrontier", bson.D{{"$gte", longitude}}}},
+			},
+		},
+	}
+	// find current location
+	var results interfaces.Zone
+	err := ZonesCollection.FindOne(
+		context.Background(),
+		filter).Decode(&results)
+
+	if err != nil {
+		return interfaces.Zone{}, err
+	}
+
+	/* if err = currLocation.All(context.TODO(), &results); err != nil {
+		panic(err)
+	} */
+
+	/* filterNearZones := bson.D{
+		{"$and",
+			bson.A{
+				bson.D{{"rightFrontier", bson.D{{"$lte", (latitude + 0.0035)}}}},
+				bson.D{{"leftFrontier", bson.D{{"$gte", (latitude + 0.0035)}}}},
+				bson.D{{"bottomFrontier", bson.D{{"$gte", (longitude + 0.0035)}}}},
+				bson.D{{"topFrontier", bson.D{{"$lte", (longitude + 0.0035)}}}},
+			},
+		},
+	} */
+
+	/* for _, result := range results {
+		res, _ := json.Marshal(result)
+
+		fmt.Println(result)
+		fmt.Println(string(res))
+	} */
+
+	return results, err
+
+}
