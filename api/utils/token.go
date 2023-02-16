5a6f4602c435bcbc43101f19ab2c81158c69cd37
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-16 17:28:16 -0500

    refactor(Login): Update JSON response

--- api/utils/token.go
+++ api/utils/token.go
@@ -8,51 +8,42 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-// secret key
+// secret keys
 var JWTKeyAC = []byte(os.Getenv("SECRET_AC"))
 var JWTKeyRF = []byte(os.Getenv("SECRET_RF"))
 
-// create jwt, accessToken and refreshToken
-
-func CreateAccessToken(userID string) (string, *jwt.Token, error) {
-
-	// 1 month
+func CreateAccessToken(userID string) (string, error) {
+	// 30 minutes short lived token
 	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"userid":    userID,
 		"notBefore": time.Now(),
 		"expire":    time.Now().Add(time.Minute * 30),
 	})
 
-	// sign with secret
-	// and get encoded token
-
+	// sign with secret and get encoded token
 	var err error
 	accessTokenString, err := accessToken.SignedString(JWTKeyAC)
 	if err != nil {
-		return "", nil, errors.New("Could not create access token")
+		return "", errors.New("Could not create access token")
 	}
 
-	return accessTokenString, accessToken, nil
+	return accessTokenString, nil
 }
 
-func CreateRefreshToken(userID string) (string, *jwt.Token, error) {
-
-	// 31 day
+func CreateRefreshToken(userID string) (string, error) {
+	// 5 months long lived token
 	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"userid":    userID,
 		"notBefore": time.Now(),
 		"expire":    time.Now().AddDate(0, 5, 0),
 	})
 
-	// sign with secret
-	// and get encoded token
-
+	// sign with secret and get encoded token
 	var err error
-
 	refreshTokenString, err := refreshToken.SignedString(JWTKeyRF)
 	if err != nil {
-		return "", nil, errors.New("Could not create refresh token")
+		return "", errors.New("Could not create refresh token")
 	}
 
-	return refreshTokenString, refreshToken, nil
+	return refreshTokenString, nil
 }
