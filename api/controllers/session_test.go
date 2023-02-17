ea4e3f35d09f887ab49b2c74a7dc11ba211af1f1
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-17 09:14:30 -0500

    :white_check_mark: Test success case for `/login` route

--- api/controllers/session_test.go
+++ api/controllers/session_test.go
@@ -41,3 +41,38 @@ func TestLoginForbidden(t *testing.T) {
 	// Delete the user from the database
 	usersCollection.DeleteOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}})
 }
+
+func TestLoginSuccess(t *testing.T) {
+	c := require.New(t)
+	var response map[string]string
+	ctx := context.Background()
+	defer ctx.Done()
+
+	// Create a random user
+	randomUser := tests.GenerateRandomUser()
+	router := tests.SetupGinRouter()
+	tests.InsertUser(randomUser, router, HandleSignUp)
+
+	// Verify the user
+	usersCollection.UpdateOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}}, bson.D{{Key: "$set", Value: bson.D{{Key: "isVerified", Value: true}}}})
+
+	// Try to login with the random user
+	loginForm := map[string]string{
+		"email":    randomUser.Email,
+		"password": randomUser.Password,
+	}
+
+	router.POST("/login", HandleLogIn)
+	w, req := tests.SetupPostRequest("/login", loginForm)
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	// Check if the response is correct
+	c.Equal(http.StatusOK, w.Code)
+	c.Equal("Successfully logged in", response["message"])
+	c.NotEmpty(response["accessToken"])
+	c.NotEmpty(response["refreshToken"])
+
+	// Delete the user from the database
+	usersCollection.DeleteOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}})
+}
