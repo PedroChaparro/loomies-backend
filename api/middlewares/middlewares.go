861a07a428ba5ed86cc4e2db820aaa416315013e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-18 10:49:17 -0500

    :sparkles: feat(session): Middleware to check access token

---
+++ api/middlewares/middlewares.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/PedroChaparro/loomies-backend/utils"
+	"github.com/gin-gonic/gin"
+)
+
+// MustProvideAccessToken checks if a valid access token was provided in the header
+func MustProvideAccessToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get access token from header
+		accessToken := c.GetHeader("Access-Token")
+
+		if accessToken == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Access token is required"})
+			return
+		}
+
+		// Check if access token is valid
+		id, error := utils.ValidateAccessToken(accessToken)
+		if error != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": error.Error()})
+			return
+		}
+
+		// Set user id to context
+		c.Set("userid", id)
+	}
+}
