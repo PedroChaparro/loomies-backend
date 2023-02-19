6c54e2a256dd45298b9324e18beea5952cb5f24d
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-18 20:13:57 -0500

    Revert "tests(refresh): Test success case for `/refresh`"
    This reverts commit dffffddba00b005fb1ca05258e5a4b895fd8c9a1.

--- api/controllers/session_test.go
+++ api/controllers/session_test.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/PedroChaparro/loomies-backend/configuration"
 	"github.com/PedroChaparro/loomies-backend/interfaces"
-	"github.com/PedroChaparro/loomies-backend/middlewares"
 	"github.com/PedroChaparro/loomies-backend/tests"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/stretchr/testify/require"
@@ -98,57 +97,5 @@ func TestLoginSuccess(t *testing.T) {
 	c.Equal(databaseUser.Id.Hex(), refreshTokenClaims["userid"])
 
 	// Delete the user from the database
-	usersCollection.DeleteOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}})
-}
-
-func TestWhoamiSuccess(t *testing.T) {
-	c := require.New(t)
-	ctx := context.Background()
-	defer ctx.Done()
-
-	// Create a random user
-	randomUser := tests.GenerateRandomUser()
-	router := tests.SetupGinRouter()
-	tests.InsertUser(randomUser, router, HandleSignUp)
-
-	// Verify the user and save the database document
-	usersCollection.UpdateOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}}, bson.D{{Key: "$set", Value: bson.D{{Key: "isVerified", Value: true}}}})
-	var databaseUser interfaces.User
-	usersCollection.FindOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}}).Decode(&databaseUser)
-
-	// Login with the random user
-	var loginResponse map[string]interface{}
-	loginForm := map[string]string{
-		"email":    randomUser.Email,
-		"password": randomUser.Password,
-	}
-
-	router.POST("/login", HandleLogIn)
-	w, req := tests.SetupPostRequest("/login", loginForm)
-	router.ServeHTTP(w, req)
-	json.Unmarshal(w.Body.Bytes(), &loginResponse)
-
-	// Get a new access token from the refresh token
-	var refreshResponse map[string]string
-	router.POST("/refresh", middlewares.MustProvideRefreshToken(), HandleRefresh)
-	w, req = tests.SetupPostRequest("/refresh", nil, tests.CustomHeader{Name: "Refresh-Token", Value: loginResponse["refreshToken"].(string)})
-	router.ServeHTTP(w, req)
-	json.Unmarshal(w.Body.Bytes(), &refreshResponse)
-
-	// 1. Check if the response fields are correct
-	c.Equal(http.StatusOK, w.Code)
-	c.NotEmpty(refreshResponse["accessToken"])
-	c.Equal("Successfully refreshed access token", refreshResponse["message"])
-
-	// 2. Check tokens claims
-	accessTokenClaims := jwt.MapClaims{}
-	_, err := jwt.ParseWithClaims(refreshResponse["accessToken"], accessTokenClaims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(configuration.GetAccessTokenSecret()), nil
-	})
-
-	c.NoError(err)
-	c.Equal(databaseUser.Id.Hex(), accessTokenClaims["userid"])
-
-	// Remove the user from the database
 	usersCollection.DeleteOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}})
 }
