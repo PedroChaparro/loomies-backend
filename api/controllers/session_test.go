5e46c6d252e3b722eb3e7c1a8a7f338f8c38e1ac
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-18 20:13:54 -0500

    Revert "chore(tests): Rename test"
    This reverts commit 8d5d6f7b0f45ddcc96cb6099a16f06421f132d74.

--- api/controllers/session_test.go
+++ api/controllers/session_test.go
@@ -133,8 +133,8 @@ func TestLoginSuccess(t *testing.T) {
 	usersCollection.DeleteOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}})
 }
 
-// TestRefreshSuccess tests the whoami endpoint with a valid access token
-func TestRefreshSuccess(t *testing.T) {
+// TestWhoamiSuccess tests the whoami endpoint with a valid access token
+func TestWhoamiSuccess(t *testing.T) {
 	c := require.New(t)
 	ctx := context.Background()
 	defer ctx.Done()
