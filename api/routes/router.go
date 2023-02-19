e2335c258e7c5530ad8ade173d92d2e6349fdd47
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-18 20:55:18 -0500

    Trigger ci workflow

--- api/routes/router.go
+++ api/routes/router.go
@@ -1,12 +1,15 @@
 package routes
 
 import (
+	"fmt"
+
 	"github.com/PedroChaparro/loomies-backend/controllers"
 	"github.com/PedroChaparro/loomies-backend/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(engine *gin.Engine) {
+	fmt.Println("Setting up routes...")
 	engine.POST("/signup", controllers.HandleSignUp)
 	engine.POST("/login", controllers.HandleLogIn)
 	engine.GET("/whoami", middlewares.MustProvideAccessToken(), controllers.HandleWhoami)
