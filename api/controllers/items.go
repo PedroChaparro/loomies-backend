cc9a8520589a02e4e4aa86f9e7dc8aad9625bf7d
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2023-03-03 15:57:23 -0500

    Add error mongo

--- api/controllers/items.go
+++ api/controllers/items.go
@@ -29,8 +29,13 @@ func HandleGetItems(c *gin.Context) {
 		id := user.Items[i].Id
 		item, err := models.GetItemById(id)
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
-			return
+			if err == mongo.ErrNoDocuments {
+				c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Item was not found"})
+				return
+			} else {
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+				return
+			}
 		}
 		data := interfaces.GetItem{Id: item.Id, Name: item.Name, Description: item.Description, Target: item.Target, Is_combat_item:item.Is_combat_item, Quantity:user.Items[i].Quantity}
 		user_items = append(user_items, data)
