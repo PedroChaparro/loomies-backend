069379b53c23039a00ef414bad4202678c8bc890
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-03-10 16:37:57 -0500

    refactor: ♻️ Refactor rewards generation
    Create helper functions to avoid repeating code

--- algorithms/database/cronjobs/update_gyms_rewards/update.js
+++ algorithms/database/cronjobs/update_gyms_rewards/update.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import Randomly from "weighted-randomly-select";
+import { getRandomRewardsAmount, generateRewards } from "./helpers.js";
 import { GymModel, ItemModel, LoomBallModel } from "../../models/mongoose.js";
 
 // Connect to MongoDB
@@ -22,9 +22,6 @@ async function removeRewardsAndClaimers() {
 }
 
 // 2. Generate & save the new rewards
-function getRandomRewardsAmount(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
 async function generateNewRewards() {
   // Get all the items and loomballs
@@ -69,67 +66,25 @@ async function generateNewRewards() {
     const player_generated_rewards = [];
     const owner_generated_rewards = [];
 
-    // -- Generate rewards for the players
-    for (let i = 0; i < rewards_qty; i++) {
-      const player_sel = Randomly.select(current_player_chances);
-      const min_qty = player_sel.min_reward_quantity;
-      const max_qty = player_sel.max_reward_quantity;
-      const qty = Math.floor(Math.random() * (max_qty - min_qty + 1) + min_qty);
+    // Generate rewards for the players
+    await generateRewards(
+      3,
+      5,
+      players_chances,
+      items,
+      player_generated_rewards,
+      PLAYERS_GENERATED_REWARDS
+    );
 
-      // Add to the gym rewards
-      const isItem = items.some((item) => item._doc._id === player_sel._id);
-
-      player_generated_rewards.push({
-        reward_collection: isItem ? "items" : "loom_balls",
-        reward_id: player_sel._id,
-        reward_quantity: qty,
-      });
-
-      // Add to the debug variable
-      PLAYERS_GENERATED_REWARDS[player_sel.name] = PLAYERS_GENERATED_REWARDS[
-        player_sel.name
-      ]
-        ? PLAYERS_GENERATED_REWARDS[player_sel.name] + qty
-        : qty;
-
-      // Remove from the chances array
-      current_player_chances = current_player_chances.filter(
-        (reward) => reward.result._id !== player_sel._id
-      );
-    }
-
-    // -- Generate rewards for the owners
-    min_rewards_qty = 5;
-    max_rewards_qty = 7;
-    rewards_qty = getRandomRewardsAmount(min_rewards_qty, max_rewards_qty);
-
-    for (let i = 0; i < rewards_qty; i++) {
-      const owner_sel = Randomly.select(current_owner_chances);
-      const min_qty = owner_sel.min_reward_quantity;
-      const max_qty = owner_sel.max_reward_quantity;
-      const qty = Math.floor(Math.random() * (max_qty - min_qty + 1) + min_qty);
-
-      // Add to the gym rewards
-      const isItem = items.some((item) => item._doc._id === owner_sel._id);
-
-      owner_generated_rewards.push({
-        reward_collection: isItem ? "items" : "loom_balls",
-        reward_id: owner_sel._id,
-        reward_quantity: qty,
-      });
-
-      // Add to the debug variable
-      OWNERS_GENERATED_REWARDS[owner_sel.name] = OWNERS_GENERATED_REWARDS[
-        owner_sel.name
-      ]
-        ? OWNERS_GENERATED_REWARDS[owner_sel.name] + qty
-        : qty;
-
-      // Remove from the chances array
-      current_owner_chances = current_owner_chances.filter(
-        (reward) => reward.result._id !== owner_sel._id
-      );
-    }
+    // Generate rewards for the owners
+    await generateRewards(
+      4,
+      6,
+      owners_chances,
+      items,
+      owner_generated_rewards,
+      OWNERS_GENERATED_REWARDS
+    );
 
     // Save the rewards
     await GymModel.updateOne(
