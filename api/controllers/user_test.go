9a886a2adc9dc2cb15c63279304f3bef19becbf7
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-04-03 07:30:56 -0500

    test(user): ✅ Test `/user/loomies` endpoint

--- api/controllers/user_test.go
+++ api/controllers/user_test.go
@@ -10,14 +10,18 @@ import (
 
 	"github.com/PedroChaparro/loomies-backend/configuration"
 	"github.com/PedroChaparro/loomies-backend/interfaces"
+	"github.com/PedroChaparro/loomies-backend/middlewares"
 	"github.com/PedroChaparro/loomies-backend/tests"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // ### Global variables ###
 var usersCollection = configuration.ConnectToMongoCollection("users")
 var authenticationCodesCollection = configuration.ConnectToMongoCollection("authentication_codes")
+var caughtLoomiesCollection = configuration.ConnectToMongoCollection("caught_loomies")
 
 // ### Tests ###
 // TestSignupSuccessAndConflict Tests the signup endpoint with a valid payload and a payload with an already existing email / username
@@ -40,7 +44,9 @@ func TestSignupSuccessAndConflict(t *testing.T) {
 	var response map[string]string
 	json.Unmarshal(w.Body.Bytes(), &response)
 
+	// -------------------------
 	// 1. Success request
+	// -------------------------
 	c.NoError(err)
 	c.Equal(http.StatusOK, w.Code)
 	c.Equal("User created successfully", response["message"])
@@ -52,7 +58,9 @@ func TestSignupSuccessAndConflict(t *testing.T) {
 	c.Equal(payload.Email, user.Email)
 	c.Equal(payload.Username, user.Username)
 
+	// -------------------------
 	// 2. Conflict request with the same email
+	// -------------------------
 	w, req = tests.SetupPostRequest("/user/signup", payload)
 	router.ServeHTTP(w, req)
 	json.Unmarshal(w.Body.Bytes(), &response)
@@ -61,7 +69,9 @@ func TestSignupSuccessAndConflict(t *testing.T) {
 	c.Equal(http.StatusConflict, w.Code)
 	c.Equal("Email already exists", response["message"])
 
+	// -------------------------
 	// 3. Conflict request with the same username
+	// -------------------------
 	oldEmail := payload.Email
 	payload.Email = tests.FakerInstance.Internet().Email()
 	w, req = tests.SetupPostRequest("/user/signup", payload)
@@ -119,7 +129,9 @@ func TestAccountValidationCodeBadRequest(t *testing.T) {
 	router := tests.SetupGinRouter()
 	tests.InsertUser(randomUser, router, HandleSignUp)
 
+	// -------------------------
 	// 1. Test without JSON payload
+	// -------------------------
 	router.POST("/user/validate/code", HandleAccountValidationCodeRequest)
 	w, req := tests.SetupPostRequest("/user/validate/code", nil)
 	router.ServeHTTP(w, req)
@@ -129,7 +141,9 @@ func TestAccountValidationCodeBadRequest(t *testing.T) {
 	c.Equal(true, response["error"])
 	c.Equal("Bad request", response["message"])
 
+	// -------------------------
 	// 2. Test with empty email
+	// -------------------------
 	w, req = tests.SetupPostRequest("/user/validate/code", map[string]string{"email": ""})
 	router.ServeHTTP(w, req)
 	json.Unmarshal(w.Body.Bytes(), &response)
@@ -138,7 +152,9 @@ func TestAccountValidationCodeBadRequest(t *testing.T) {
 	c.Equal(true, response["error"])
 	c.Equal("Email cannot be empty", response["message"])
 
+	// -------------------------
 	// 3. Test with unregistred email
+	// -------------------------
 	w, req = tests.SetupPostRequest("/user/validate/code", map[string]string{"email": "unexisting@gmail.com"})
 	router.ServeHTTP(w, req)
 	json.Unmarshal(w.Body.Bytes(), &response)
@@ -147,7 +163,9 @@ func TestAccountValidationCodeBadRequest(t *testing.T) {
 	c.Equal(true, response["error"])
 	c.Equal("This Email has not been registered", response["message"])
 
+	// -------------------------
 	// 4. Test with verified email
+	// -------------------------
 	usersCollection.UpdateOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}}, bson.D{{Key: "$set", Value: bson.D{{Key: "isVerified", Value: true}}}})
 	w, req = tests.SetupPostRequest("/user/validate/code", map[string]string{"email": randomUser.Email})
 	router.ServeHTTP(w, req)
@@ -209,7 +227,9 @@ func TestAccountValidationBadRequest(t *testing.T) {
 	c.NoError(err)
 	codeNumber, _ := strconv.Atoi(code.Code)
 
+	// -------------------------
 	// 1. Test without JSON payload
+	// -------------------------
 	router.POST("/user/validate", HandleAccountValidation)
 	w, req := tests.SetupPostRequest("/user/validate", nil)
 	router.ServeHTTP(w, req)
@@ -219,7 +239,9 @@ func TestAccountValidationBadRequest(t *testing.T) {
 	c.Equal(true, response["error"])
 	c.Equal("Bad request", response["message"])
 
+	// -------------------------
 	// 2. Test with empty email
+	// -------------------------
 	w, req = tests.SetupPostRequest("/user/validate", map[string]string{"email": "", "validationCode": code.Code})
 	router.ServeHTTP(w, req)
 	json.Unmarshal(w.Body.Bytes(), &response)
@@ -228,7 +250,9 @@ func TestAccountValidationBadRequest(t *testing.T) {
 	c.Equal(true, response["error"])
 	c.Equal("Email cannot be empty", response["message"])
 
+	// -------------------------
 	// 3. Test with empty validationCode
+	// -------------------------
 	w, req = tests.SetupPostRequest("/user/validate", map[string]string{"email": randomUser.Email, "validationCode": ""})
 	router.ServeHTTP(w, req)
 	json.Unmarshal(w.Body.Bytes(), &response)
@@ -237,7 +261,9 @@ func TestAccountValidationBadRequest(t *testing.T) {
 	c.Equal(true, response["error"])
 	c.Equal("Verification code cannot be empty", response["message"])
 
+	// -------------------------
 	// 4. Test with incorrect validationCode
+	// -------------------------
 	w, req = tests.SetupPostRequest("/user/validate", map[string]string{"email": randomUser.Email, "validationCode": strconv.Itoa(codeNumber + 1)})
 	router.ServeHTTP(w, req)
 	json.Unmarshal(w.Body.Bytes(), &response)
@@ -248,5 +274,80 @@ func TestAccountValidationBadRequest(t *testing.T) {
 
 	// Remove the user
 	err = tests.DeleteUser(randomUser.Email)
+	c.NoError(err)
+}
+
+// TestGetLoomiesSuccess Test the success case for /user/loomies endpoint
+func TestGetLoomiesSuccess(t *testing.T) {
+	var response map[string]interface{}
+	c := require.New(t)
+	ctx := context.Background()
+	defer ctx.Done()
+
+	// Create a random user
+	router := tests.SetupGinRouter()
+	randomUser, loginResponse := loginWithRandomUser()
+	token := loginResponse["accessToken"]
+
+	// -------------------------
+	// 1. Test with no loomies
+	// -------------------------
+	router.GET("/user/loomies", middlewares.MustProvideAccessToken(), HandleGetLoomies)
+	w, req := tests.SetupGetRequest("/user/loomies", tests.CustomHeader{Name: "Access-Token", Value: token})
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	c.Equal(http.StatusOK, w.Code)
+	c.Equal(false, response["error"])
+	c.Equal(0, len(response["loomies"].([]interface{})))
+
+	// -------------------------
+	// 2. Test with some loomies
+	// -------------------------
+	// Get the first 6 loomies from the caught_loomies collection
+	var loomies []interfaces.CaughtLoomie
+	var loomiesIds []primitive.ObjectID
+	cursor, _ := caughtLoomiesCollection.Find(ctx, bson.D{}, options.Find().SetLimit(6))
+	cursor.All(ctx, &loomies)
+	c.Equal(6, len(loomies))
+
+	// Get the ids of the loomies
+	for _, loomie := range loomies {
+		loomiesIds = append(loomiesIds, loomie.Id)
+	}
+
+	// Insert the loomies into the user's loomies
+	usersCollection.UpdateOne(ctx, bson.D{{Key: "email", Value: randomUser.Email}}, bson.D{{Key: "$set", Value: bson.D{{Key: "loomies", Value: loomiesIds}}}})
+
+	// Make the request and get the JSON response
+	w, req = tests.SetupGetRequest("/user/loomies", tests.CustomHeader{Name: "Access-Token", Value: token})
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	c.Equal(http.StatusOK, w.Code)
+	c.Equal(false, response["error"])
+	c.Equal(6, len(response["loomies"].([]interface{})))
+
+	// Check the loomies fields
+	for _, loomie := range response["loomies"].([]interface{}) {
+		loomie := loomie.(map[string]interface{})
+		c.NotEmpty(loomie["_id"])
+		c.NotEmpty(loomie["serial"])
+		c.NotEmpty(loomie["name"])
+		c.NotEmpty(loomie["rarity"])
+		c.NotEmpty(loomie["hp"])
+		c.NotEmpty(loomie["attack"])
+		c.NotEmpty(loomie["defense"])
+		c.NotEmpty(loomie["is_busy"])
+		// Loomies cannot have less than 1 type
+		c.NotEmpty(loomie["types"])
+		c.GreaterOrEqual(len(loomie["types"].([]interface{})), 1)
+		// Default level is 1
+		c.NotEmpty(loomie["level"])
+		c.GreaterOrEqual(int(loomie["level"].(float64)), 1)
+	}
+
+	// Remove the user
+	err := tests.DeleteUser(randomUser.Email)
 	c.NoError(err)
 }
