fde67d440ef28901f1e5fdbacdcff2709307cbdc
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-04-06 11:57:41 -0500

    test(gyms): ✅ Test `/gyms/claim-rewards` endpoint

--- api/controllers/gyms_test.go
+++ api/controllers/gyms_test.go
@@ -173,3 +173,263 @@ func TestGymDetailsBadRequest(t *testing.T) {
 	err := tests.DeleteUser(randomUser.Email, randomUser.Id)
 	c.NoError(err)
 }
+
+// TestClaimGymRewardsSuccess Tests the `/gyms/claim-rewards“ endpoint with a success response
+func TestClaimGymRewardsSuccess(t *testing.T) {
+	var response map[string]interface{}
+	c := require.New(t)
+	ctx := context.Background()
+	defer ctx.Done()
+
+	// Login with a random user
+	randomUser, loginResponse := loginWithRandomUser()
+
+	// Get an existing gym
+	var gym interfaces.Gym
+	err := models.GymsCollection.FindOne(ctx, bson.M{}).Decode(&gym)
+	c.NoError(err)
+
+	// -------------------------
+	// 1. Test with user rewards
+	// -------------------------
+
+	// Setup the router
+	router := tests.SetupGinRouter()
+	router.POST("/gyms/claim-rewards", middlewares.MustProvideAccessToken(), HandleClaimReward)
+	w, req := tests.SetupPayloadedRequest("/gyms/claim-rewards", "POST", map[string]interface{}{
+		"gym_id":    gym.Id.Hex(),
+		"latitude":  gym.Latitude,
+		"longitude": gym.Longitude,
+	}, tests.CustomHeader{
+		Name:  "Access-Token",
+		Value: loginResponse["accessToken"],
+	})
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	// Check the fields
+	c.Equal(200, w.Code)
+	c.Equal(false, response["error"])
+	c.Equal("Reward claimed successfully", response["message"])
+	c.NotEmpty(response["reward"])
+
+	// Check rewards fields
+	var userRewardsIds []string
+	rewards := response["reward"].([]interface{})
+
+	for _, reward := range rewards {
+		reward := reward.(map[string]interface{})
+		t.Log(reward)
+		c.NotEmpty(reward["id"])
+		c.NotEmpty(reward["name"])
+		c.Positive(reward["quantity"])
+		c.NotEmpty(reward["serial"])
+		c.Positive(reward["serial"])
+		userRewardsIds = append(userRewardsIds, reward["id"].(string))
+	}
+
+	// Check rewards ids to match with the user rewards on the database
+	c.Equal(len(rewards), len(userRewardsIds))
+	for _, reward := range rewards {
+		c.Contains(userRewardsIds, reward.(map[string]interface{})["id"])
+	}
+
+	// -------------------------
+	// 2. Test with owner rewards
+	// -------------------------
+
+	// Update the gym's owner
+	_, err = models.GymsCollection.UpdateByID(ctx, gym.Id, bson.M{
+		"$set": bson.M{
+			"owner": randomUser.Id,
+		},
+		"$pull": bson.M{
+			"rewards_claimed_by": randomUser.Id,
+		},
+	})
+	c.NoError(err)
+
+	// Make the request
+	w, req = tests.SetupPayloadedRequest("/gyms/claim-rewards", "POST", map[string]interface{}{
+		"gym_id":    gym.Id.Hex(),
+		"latitude":  gym.Latitude,
+		"longitude": gym.Longitude,
+	}, tests.CustomHeader{
+		Name:  "Access-Token",
+		Value: loginResponse["accessToken"],
+	})
+
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	// Check the fields
+	c.Equal(200, w.Code)
+	c.Equal(false, response["error"])
+	c.Equal("Reward claimed successfully", response["message"])
+	c.NotEmpty(response["reward"])
+
+	// Check rewards fields
+	var ownerRewardsIds []string
+	rewards = response["reward"].([]interface{})
+
+	for _, reward := range rewards {
+		reward := reward.(map[string]interface{})
+		t.Log(reward)
+		c.NotEmpty(reward["id"])
+		c.NotEmpty(reward["name"])
+		c.Positive(reward["quantity"])
+		c.NotEmpty(reward["serial"])
+		c.Positive(reward["serial"])
+		ownerRewardsIds = append(ownerRewardsIds, reward["id"].(string))
+	}
+
+	// Check rewards ids to match with the owners rewards on the database
+	c.Equal(len(rewards), len(ownerRewardsIds))
+	for _, reward := range rewards {
+		c.Contains(ownerRewardsIds, reward.(map[string]interface{})["id"])
+	}
+
+	// Delete the user
+	err = tests.DeleteUser(randomUser.Email, randomUser.Id)
+	c.NoError(err)
+}
+
+// TestClaimRewardsBadRequest Tests the `/gyms/claim-rewards“ endpoint with a bad request
+func TestClaimRewardsBadRequest(t *testing.T) {
+	var response map[string]interface{}
+	c := require.New(t)
+	ctx := context.Background()
+	defer ctx.Done()
+
+	// Login with a random user
+	randomUser, loginResponse := loginWithRandomUser()
+
+	// Get an existing gym
+	var gym interfaces.Gym
+	err := models.GymsCollection.FindOne(ctx, bson.M{}).Decode(&gym)
+	c.NoError(err)
+
+	// Setup the router
+	router := tests.SetupGinRouter()
+	router.POST("/gyms/claim-rewards", middlewares.MustProvideAccessToken(), HandleClaimReward)
+
+	// -------------------------
+	// 1. Check with an empty payload
+	// -------------------------
+	w, req := tests.SetupPayloadedRequest("/gyms/claim-rewards", "POST", nil, tests.CustomHeader{
+		Name:  "Access-Token",
+		Value: loginResponse["accessToken"],
+	})
+
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	c.Equal(400, w.Code)
+	c.Equal(true, response["error"])
+	c.Equal("Invalid request body", response["message"])
+
+	// -------------------------
+	// 2. Check with empty fields
+	// -------------------------
+	w, req = tests.SetupPayloadedRequest("/gyms/claim-rewards", "POST", map[string]interface{}{
+		"latitude":  gym.Latitude,
+		"longitude": gym.Longitude,
+	}, tests.CustomHeader{
+		Name:  "Access-Token",
+		Value: loginResponse["accessToken"],
+	})
+
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	c.Equal(400, w.Code)
+	c.Equal(true, response["error"])
+	c.Equal("Gym id, latitude and longitude are required", response["message"])
+
+	w, req = tests.SetupPayloadedRequest("/gyms/claim-rewards", "POST", map[string]interface{}{
+		"gym_id": gym.Id.Hex(),
+	}, tests.CustomHeader{
+		Name:  "Access-Token",
+		Value: loginResponse["accessToken"],
+	})
+
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	c.Equal(400, w.Code)
+	c.Equal(true, response["error"])
+	c.Equal("Gym id, latitude and longitude are required", response["message"])
+
+	// -------------------------
+	// 3. Check with non-existing gym id
+	// -------------------------
+	w, req = tests.SetupPayloadedRequest("/gyms/claim-rewards", "POST", map[string]interface{}{
+		"gym_id":    "5f6b9c1b9c9c9c9c9c9c9c9c",
+		"latitude":  gym.Latitude,
+		"longitude": gym.Longitude,
+	}, tests.CustomHeader{
+		Name:  "Access-Token",
+		Value: loginResponse["accessToken"],
+	})
+
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	c.Equal(404, w.Code)
+	c.Equal(true, response["error"])
+	c.Equal("Gym not found", response["message"])
+
+	// -------------------------
+	// 4. Check with away coordinates
+	// -------------------------
+	w, req = tests.SetupPayloadedRequest("/gyms/claim-rewards", "POST", map[string]interface{}{
+		"gym_id":    gym.Id.Hex(),
+		"latitude":  gym.Latitude + 0.0036,
+		"longitude": gym.Longitude + 0.0036,
+	}, tests.CustomHeader{
+		Name:  "Access-Token",
+		Value: loginResponse["accessToken"],
+	})
+
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	c.Equal(400, w.Code)
+	c.Equal(true, response["error"])
+	c.Equal("You are too far from the gym", response["message"])
+
+	// -------------------------
+	// 5. Check with already claimed rewards
+	// -------------------------
+	// Insert the user in the gym `rewards_claimed_by` array
+	_, err = models.GymsCollection.UpdateOne(ctx, bson.M{
+		"_id": gym.Id,
+	}, bson.M{
+		"$push": bson.M{
+			"rewards_claimed_by": randomUser.Id,
+		},
+	})
+
+	c.NoError(err)
+
+	// Make the request
+	w, req = tests.SetupPayloadedRequest("/gyms/claim-rewards", "POST", map[string]interface{}{
+		"gym_id":    gym.Id.Hex(),
+		"latitude":  gym.Latitude,
+		"longitude": gym.Longitude,
+	}, tests.CustomHeader{
+		Name:  "Access-Token",
+		Value: loginResponse["accessToken"],
+	})
+
+	router.ServeHTTP(w, req)
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	c.Equal(400, w.Code)
+	c.Equal(true, response["error"])
+	c.Equal("You already claimed the rewards for this gym", response["message"])
+
+	// Delete the user
+	err = tests.DeleteUser(randomUser.Email, randomUser.Id)
+	c.NoError(err)
+}
